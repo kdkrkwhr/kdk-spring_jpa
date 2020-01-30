6c2da46f5a6eebe35cf9d080409750d04b4dda5d
KimDongki <kdkdongki1997@gmail.com>
2020-01-30 14:12:20 +0900

    config 파일 암호화 적용

--- src/main/java/com/example/RestApiApplication.java
+++ src/main/java/com/example/RestApiApplication.java
@@ -1,12 +1,15 @@
 package com.example;
 
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 
+@EnableEncryptableProperties
 @SpringBootApplication
 public class RestApiApplication {
 	
@@ -15,6 +18,9 @@ public class RestApiApplication {
 		return new ApplicationRunner() {
 			@Override
 			public void run(ApplicationArguments args) throws Exception {
+				StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
+				pbeEnc.setAlgorithm("PBEWithMD5AndDES");
+				pbeEnc.setPassword("kdk");
 			}
 		};
 	}
